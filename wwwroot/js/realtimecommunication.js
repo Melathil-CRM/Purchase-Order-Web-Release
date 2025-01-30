37f89fe30cc461795dd5203955b8a44bbe75e9e2
Jithin S <s.jithin@outlook.com>
2025-01-31 00:54:47 +0530

    1.0.1.6

---
+++ wwwroot/js/realtimecommunication.js
@@ -0,0 +1,14 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/pohub").build();
+connection.on("ReceiveMessage", function (user, message) {
+    console.log("One message received");
+});
+connection.on("PurchaseOrderCreate", function (user, message) {
+    console.log("Purchase Order Created");
+});
+
+connection.start().then(function () {
+    console.log("Signal R Connection Established")
+}).catch(function (err) {
+    console.log("Signal R Connection Failed")
+    return console.error(err.toString());
+});
